258afce5189194ace26d7dfca55cad89808eb22b
Yonatan Yerushalmy <yonyerush@gmail.com>
2025-05-29 17:53:08 +0300

    Update mutants_compare_delta_plot.r

--- mutants_compare_delta_plot.r
+++ mutants_compare_delta_plot.r
@@ -3,6 +3,7 @@ library(dplyr)
 library(GenomicRanges)
 
 source("https://raw.githubusercontent.com/Yo-yerush/general_scripts/main/windowSize_for_GRanges_mcol.r")
+source("https://raw.githubusercontent.com/Yo-yerush/general_scripts/main/ChrPlots_CX_from_GRanges.R")
 
 read_mut_file <- function(mut_name) {
     x <- read.csv("paste0(PATH/TO/mutants_figs/", mut_name, "_delta_df.csv.gz"))
@@ -20,7 +21,5 @@ cmt3 <- read_mut_file("cmt3")
 ddm1 <- read_mut_file("ddm1")
 
 ChrPlots_CX("test_stroud", list(met1$cg,cmt2$cg,cmt3$cg,ddm1$cg), c("met1","cmt2","cmt3","ddm1"), "CG", y_max = 0, y_mid = 0, y_min = -1, output_dir="PATH/TO/mutants_figs")
-
 ChrPlots_CX("test_stroud", list(met1$chg,cmt2$chg,cmt3$chg,ddm1$chg), c("met1","cmt2","cmt3","ddm1"), "CHG", y_max = 0.2, y_mid = 0, y_min = -0.5, output_dir="PATH/TO/mutants_figs")
-
 ChrPlots_CX("test_stroud", list(met1$chh,cmt2$chh,cmt3$chh,ddm1$chh), c("met1","cmt2","cmt3","ddm1"), "CHH", y_max = 0.05, y_mid = 0, y_min = -0.15, output_dir="PATH/TO/mutants_figs")
