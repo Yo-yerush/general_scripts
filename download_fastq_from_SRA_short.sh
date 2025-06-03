ac4c8368da6fca8ce4c172c58d748c96256098f7
Yonatan Yerushalmy <yonyerush@gmail.com>
2025-06-03 16:19:29 +0300

    Update download_fastq_from_SRA_short.sh

--- download_fastq_from_SRA_short.sh
+++ download_fastq_from_SRA_short.sh
@@ -19,7 +19,7 @@ for SRR_ID in "${SRR_ARRAY[@]}"; do
     # convert the .sra file to fastq.gz
     echo "Converting '"$SRR_ID".sra' to '"$SRR_ID".fastq.gz'"
     fasterq-dump "$SRR_ID" --threads 12 -m 4G --progress --split-files
-    gzip "$SRR_ID".fastq
+    gzip "$SRR_ID"*.fastq
     
     # remove the .sra file
     echo ""
