5236c79e8e5e337b5e564ed3929aea46b4f08309
Yonatan Yerushalmy <yonyerush@gmail.com>
2025-06-18 11:54:47 +0300

    new plots

--- mto1_paper_R_scripts/linear_plot_mto_features_methylation_RNA_filtered_by_DEGs_DEseq2.r
+++ mto1_paper_R_scripts/linear_plot_mto_features_methylation_RNA_filtered_by_DEGs_DEseq2.r
@@ -7,8 +7,16 @@ library(lmerTest)
 library(tidyr)
 library(MASS)
 library(scales)
+library(hexbin)
 source("C:/Users/yonatany/Migal/Rachel Amir Team - General/yonatan/scripts/multiplot_ggplot2.R")
 source("C:/Users/yonatany/Migal/Rachel Amir Team - General/yonatan/scripts/yo_theme_base_ggplot2.R")
+
+
+# # # # # # # # # # # # # # # #
+
+make_it_light <- FALSE # if TRUE, will reduce the number of points in the plot
+
+# # # # # # # # # # # # # # # #
 
 treatment <- "mto1"
 mto_rnaseq_names <- c("met14", "met15", "met16")
@@ -25,9 +33,8 @@ main_output_directory <- "C:/Users/yonatany/Migal/Rachel Amir Team - General/yon
 ### filtered by differentially expressed genes
 DEGs_genes_list <- list(
   gene_list_name = "filtered_by_DEGs",
-  gene_list = read.csv(paste0(RNAseq_results_directory, "all.transcripts.mto1_vs_wt.DE.csv")) %>%
+  gene_list = read.csv(paste0(RNAseq_results_directory, "all_genes_results_", treatment, "_vs_wt.csv")) %>%
     filter(padj < 0.05) %>%
-    dplyr::rename(gene_id = "locus_tag") %>%
     distinct(gene_id)
 )
 
@@ -52,10 +59,15 @@ edit_ann_names <- function(x) {
   return(x)
 }
 
-# set list for all plots
+# set list for all annotation plots
 annotation_point_plots_list <- setNames(vector("list", length(annotation_type_names)), annotation_type_names)
 annotation_regression_plot_list <- setNames(vector("list", length(annotation_type_names)), annotation_type_names)
 residuals_plots_list <- setNames(vector("list", length(annotation_type_names)), annotation_type_names)
+
+# set list for all context plots
+all_cntx_points <- setNames(vector("list", 3), c("CG", "CHG", "CHH"))
+all_cntx_regression <- setNames(vector("list", 3), c("CG", "CHG", "CHH"))
+all_cntx_residuals <- setNames(vector("list", 3), c("CG", "CHG", "CHH"))
 
 ###############################
 
@@ -89,10 +101,7 @@ for (context in c("CG", "CHG", "CHH")) {
     }
 
     # Load the RNAseq '*.gene.results' files (output from RSEM pipeline)
-    RNA.0 <- read.csv(paste0(RNAseq_results_directory, "norm.", treatment, "_vs_wt.DE.csv"))
-
-    names(RNA.0)[1] <- "gene_id"
-    RNA <- RNA.0
+    RNA <- read.csv(paste0(RNAseq_results_directory, "norm_counts_", treatment, "_vs_wt.csv"))
 
     # filter by DMRs-gene_list (if exist)
     if (gene_list_name == "filtered_by_DEGs") {
@@ -200,11 +209,63 @@ for (context in c("CG", "CHG", "CHH")) {
 
         #####################################################
 
-        #### plot ####
+        #### reduce points
+        # dense_keep <- sample(nrow(plot_df), size = nrow(plot_df) * 0.75)
+        # plot_df_sub <- plot_df[dense_keep, ]
+        #
+        #
+        # hb <- hexbin(plot_df$Meth, plot_df$normCounts, xbins = 200, IDs = T, )
+        # plot_df$bin_id <- hb@cID
+        # max_per_bin <- 200
+        # plot_df_sub <- plot_df %>%
+        #  group_by(bin_id) %>%
+        #  slice_sample(n = min(nrow(plot_df), max_per_bin)) %>%
+        #  ungroup()
 
+        if (make_it_light) {
+          n_bins <- 1000
+          max_per_bin <- 100
+          plot_df$x_bin <- cut(plot_df$Meth, breaks = n_bins)
+          plot_df <- plot_df %>%
+            group_by(x_bin) %>%
+            slice_sample(n = min(nrow(plot_df), max_per_bin)) %>%
+            ungroup()
+        }
+
+        if (context == "CG") {
+          title_var <- element_text(size = 16)
+        } else {
+          title_var <- element_blank()
+        }
+
+        if (context == "CHH") {
+          xaxis_var <- element_text(size = 16)
+        } else {
+          xaxis_var <- element_blank()
+        }
+
+        if (annotation_type == "transposable_element_gene") {
+          yaxis_var <- element_blank()
+          yaxis_txt_var <- element_text(size = 13)
+        } else {
+          yaxis_var <- element_blank()
+          yaxis_txt_var <- element_blank()
+        }
+
+        if (annotation_type == "promoter") {
+          yaxis_var <- element_text(size = 16)
+          yaxis_txt_var <- element_text(size = 13)
+          context_label <- context
+        } else {
+          yaxis_var <- element_blank()
+          context_label <- ""
+        }
+
+
+        #### plot ####
         p <- ggplot(plot_df, aes(x = Meth, y = normCounts, color = genotype))
 
-        p_points <- p + geom_point(alpha = 0.2, size = 0.25) +
+        p_points <- p + geom_point(alpha = 0.6, size = 0.3) +
           stat_smooth(method = "glm.nb", formula = y ~ x) +
           scale_y_continuous(
             trans = scales::log1p_trans(),
@@ -214,15 +275,27 @@ for (context in c("CG", "CHG", "CHH")) {
             }
           ) +
           scale_color_manual(values = c("#bf6828", "gray50")) +
-          annotate("text", x = Inf, y = Inf, label = "M", hjust = 4, vjust = 2, size = 3.5) +
-          annotate("text", x = Inf, y = Inf, label = "G", hjust = 4.75, vjust = 4, size = 3.5) +
-          annotate("text", x = Inf, y = Inf, label = paste0("'", M_pval, "'"), hjust = 1.15, vjust = 2, size = 3.5) +
-          annotate("text", x = Inf, y = Inf, label = paste0("'", G_pval, "'"), hjust = 1.15, vjust = 4, size = 3.5) +
+          annotate("text", x = Inf, y = Inf, label = context_label, hjust = 1.25, vjust = 1.5, size = 6) +
+          #### updated
+          # annotate("text", x = Inf, y = Inf, label = "M", hjust = 4, vjust = 2, size = 3.5) +
+          # annotate("text", x = Inf, y = Inf, label = "G", hjust = 4.75, vjust = 4, size = 3.5) +
+          # annotate("text", x = Inf, y = Inf, label = paste0("'", M_pval, "'"), hjust = 1.15, vjust = 2, size = 3.5) +
+          # annotate("text", x = Inf, y = Inf, label = paste0("'", G_pval, "'"), hjust = 1.15, vjust = 4, size = 3.5) +
+          ####
           yo_theme_base() +
-          theme(legend.position = "none") +
+          theme(
+            legend.position = "none",
+            #### updated
+            title = title_var,
+            axis.text.x = element_text(size = 13),
+            axis.text.y = yaxis_txt_var,
+            axis.title.x = xaxis_var,
+            axis.title.y = yaxis_var
+            ####
+          ) +
           labs(
             title = edit_ann_names(annotation_type),
-            x = paste0(context, " Methylation levels"),
+            x = "Methylation level",
             y = "Norm. Counts"
           )
 
@@ -230,12 +303,22 @@ for (context in c("CG", "CHG", "CHH")) {
           scale_y_continuous(trans = scales::log1p_trans()) +
           scale_color_manual(values = c("#bf6828", "gray50")) +
           # 'M' and 'G' text
-          annotate("text", x = Inf, y = Inf, label = "M", hjust = 4, vjust = 2, size = 3.5) +
-          annotate("text", x = Inf, y = Inf, label = "G", hjust = 4.75, vjust = 4, size = 3.5) +
-          annotate("text", x = Inf, y = Inf, label = paste0("'", M_pval, "'"), hjust = 1.15, vjust = 2, size = 3.5) +
-          annotate("text", x = Inf, y = Inf, label = paste0("'", G_pval, "'"), hjust = 1.15, vjust = 4, size = 3.5) +
+          # annotate("text", x = Inf, y = Inf, label = "M", hjust = 4, vjust = 2, size = 3.5) +
+          # annotate("text", x = Inf, y = Inf, label = "G", hjust = 4.75, vjust = 4, size = 3.5) +
+          # annotate("text", x = Inf, y = Inf, label = paste0("'", M_pval, "'"), hjust = 1.15, vjust = 2, size = 3.5) +
+          # annotate("text", x = Inf, y = Inf, label = paste0("'", G_pval, "'"), hjust = 1.15, vjust = 4, size = 3.5) +
           yo_theme_base() +
-          theme(legend.position = "none") +
+          theme(
+            legend.position = "none",
+            #### updated
+            # plot.title = element_text(size = 14),
+            title = element_blank(),
+            axis.text = element_text(size = 12),
+            axis.title.x = element_blank(),
+            axis.title.y = element_blank()
+            # axis.title.y = element_text(size = 14)
+            ####
+          ) +
           labs(
             title = edit_ann_names(annotation_type),
             x = paste0(context, " Methylation levels"),
@@ -259,11 +342,11 @@ for (context in c("CG", "CHG", "CHH")) {
             x = "Fitted",
             y = "Residuals"
           )
-        
+
         message(paste0("created plot to - '", annotation_type, "' annotations in '", context, "' context successfully"))
       },
       error = function(e) {
-        message(paste0("\ncan't use 'glm.nb' and create plot to - ", annotation_type, " annotations in ", context, " context\n"))
+        message(paste0("\ncan't use 'glm.nb' and create plot to - ", annotation_type, " annotations in ", context, " context\n\n", e))
         annotation_point_plots_list[[annotation_type]] <- ggplot() +
           theme_void()
         annotation_regression_plot_list[[annotation_type]] <- ggplot() +
@@ -299,117 +382,158 @@ for (context in c("CG", "CHG", "CHH")) {
     annotation_custom(legend_p_0, xmin = 0, xmax = 0.5, ymin = 0.5, ymax = 1)
   # legend_p = grid::grid.draw(legend_p)
 
-  #### save all plot as one, for each context ####
-
-  ### main plot - with points
-  svg(paste0(path_2_save.3, "lm.stats.plot.", context, ".", treatment, ".points.svg"), width = 16, height = 2.5, family = "serif")
-  multiplot(
-    annotation_point_plots_list[[1]],
-    annotation_point_plots_list[[2]],
-    annotation_point_plots_list[[3]],
-    annotation_point_plots_list[[4]],
-    annotation_point_plots_list[[5]],
-    annotation_point_plots_list[[6]],
-    # annotation_point_plots_list[[7]],
-    # legend_p,
-    cols = 6
-  )
-  dev.off()
-
-  ### main plot - regression
-  svg(paste0(path_2_save.3, "lm.stats.plot.", context, ".", treatment, ".regression.svg"), width = 16, height = 2.5, family = "serif") # width = 12, height = 5
-  multiplot(
-    annotation_regression_plot_list[[1]],
-    annotation_regression_plot_list[[2]],
-    annotation_regression_plot_list[[3]],
-    annotation_regression_plot_list[[4]],
-    annotation_regression_plot_list[[5]],
-    annotation_regression_plot_list[[6]],
-    # annotation_regression_plot_list[[7]],
-    # legend_p,
-    cols = 6
-  )
-  dev.off()
-
-  ### residuals plot
-  svg(paste0(path_2_save.3, "lm.residuals.plot.", context, ".", treatment, ".svg"), width = 16, height = 2.5, family = "serif")
-  multiplot(
-    residuals_plots_list[[1]],
-    residuals_plots_list[[2]],
-    residuals_plots_list[[3]],
-    residuals_plots_list[[4]],
-    residuals_plots_list[[5]],
-    residuals_plots_list[[6]],
-    # residuals_plots_list[[7]],
-    # legend_p,
-    cols = 6
-  )
-  dev.off()
+  ### list of all plots from contexts ###
+  all_cntx_points[[context]] <- annotation_point_plots_list
+  all_cntx_regression[[context]] <- annotation_regression_plot_list
+  all_cntx_residuals[[context]] <- residuals_plots_list
 }
-### theta bar plot
-theta_df$annotation <- factor(theta_df$annotation, levels = unique(theta_df$annotation))
-theta_bar_plot <- ggplot(theta_df, aes(x = annotation, y = theta_value, fill = context)) +
-  geom_bar(stat = "identity", position = position_dodge(width = 0.8), width = 0.6, color = "black") +
-  geom_errorbar(aes(ymin = theta_value - theta_se, ymax = theta_value + theta_se), width = 0.2, position = position_dodge(0.8)) +
-  scale_fill_brewer(palette = "Set3") +
-  theme_bw() +
-  labs(
-    x = "",
-    y = "ϴ",
-    fill = "Context"
-  ) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, face = "bold"))
 
-svg(paste0(path_2_save.2, "theta_values_", treatment, ".svg"), width = 5, height = 3, family = "serif")
-print(theta_bar_plot)
-dev.off()
-
-### R^2 bar plot
-Rsqr_df$annotation <- factor(Rsqr_df$annotation, levels = unique(Rsqr_df$annotation))
-end_brk_1 <- max(Rsqr_df[Rsqr_df$annotation != "TEGs", ]$Rsqr_value)
-start_brk_2 <- min(c(Rsqr_df[Rsqr_df$annotation == "TEGs", ]$Rsqr_value))
-
-Rsqr_bar_plot <- ggplot(Rsqr_df, aes(x = annotation, y = Rsqr_value, fill = context)) +
-  geom_bar(stat = "identity", position = position_dodge(width = 0.8), width = 0.6, color = "black") +
-  scale_fill_brewer(palette = "Set3") +
-  theme_bw() +
-  labs(
-    x = "",
-    y = "R²",
-    fill = "Context"
-  ) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, face = "bold")) +
-  ggbreak::scale_y_break(
-    c(end_brk_1 * 1.075, start_brk_2 * 0.925),
-    space = 0.055,
-    scales = 0.5
+#### save all plot as one, for each context ####
+### main plot - with points
+png(paste0("lm.stats.plot.", treatment, ".points.png"), width = 18, height = 7.5, units = "in", res = 300, family = "serif")
+do.call(multiplot, c(
+  c(all_cntx_points$CG, all_cntx_points$CHG, all_cntx_points$CHH),
+  list(
+    layout = matrix(1:18, nrow = 3, ncol = 6, byrow = TRUE),
+  heights = c(1, 0.9, 1),
+  widths = c(1, 0.8, 0.8, 0.8, 0.8, 0.9)
   )
-
-svg(paste0(path_2_save.2, "Rsqr_values_", treatment, ".svg"), width = 5, height = 3, family = "serif")
-print(Rsqr_bar_plot)
+))
 dev.off()
 
-### Pseudo-R^2 bar plot
-ps_Rsqr_df$annotation <- factor(ps_Rsqr_df$annotation, levels = unique(ps_Rsqr_df$annotation))
-end_brk_1 <- max(ps_Rsqr_df[ps_Rsqr_df$annotation != "TEGs", ]$Rsqr_value)
-start_brk_2 <- min(c(ps_Rsqr_df[ps_Rsqr_df$annotation == "TEGs", ]$Rsqr_value))
 
-ps_Rsqr_bar_plot <- ggplot(ps_Rsqr_df, aes(x = annotation, y = Rsqr_value, fill = context)) +
-  geom_bar(stat = "identity", position = position_dodge(width = 0.8), width = 0.6, color = "black") +
-  scale_fill_brewer(palette = "Set3") +
-  theme_bw() +
-  labs(
-    x = "",
-    y = "Pseudo-R²",
-    fill = "Context"
-  ) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, face = "bold")) +
-  ggbreak::scale_y_break(
-    c(end_brk_1 * 1.075, start_brk_2 * 0.925),
-    space = 0.055,
-    scales = 0.5
-  )
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
 
-svg(paste0(path_2_save.2, "Pseudo_Rsqr_values_", treatment, ".svg"), width = 5, height = 3, family = "serif")
-print(ps_Rsqr_bar_plot)
-dev.off()
+
+
+
+
+
+
+
+
+#### main plot - with points
+#svg(paste0(path_2_save.3, "lm.stats.plot.", context, ".", treatment, ".points.svg"), width = 16, height = 2.5, family = "serif")
+#multiplot(
+#  all_cntx_points$CG,
+#  all_cntx_points$CHG,
+#  all_cntx_points$CHH,
+#  # legend_p,
+#  cols = 6,
+#  rows = 3
+#)
+#dev.off()
+#
+#### main plot - regression
+#svg(paste0(path_2_save.3, "lm.stats.plot.", context, ".", treatment, ".regression.svg"), width = 16, height = 2.5, family = "serif") # width = 12, height = 5
+#multiplot(
+#  annotation_regression_plot_list[[1]],
+#  annotation_regression_plot_list[[2]],
+#  annotation_regression_plot_list[[3]],
+#  annotation_regression_plot_list[[4]],
+#  annotation_regression_plot_list[[5]],
+#  annotation_regression_plot_list[[6]],
+#  # annotation_regression_plot_list[[7]],
+#  # legend_p,
+#  cols = 6
+#)
+#dev.off()
+#
+#### residuals plot
+#svg(paste0(path_2_save.3, "lm.residuals.plot.", context, ".", treatment, ".svg"), width = 16, height = 2.5, family = "serif")
+#multiplot(
+#  residuals_plots_list[[1]],
+#  residuals_plots_list[[2]],
+#  residuals_plots_list[[3]],
+#  residuals_plots_list[[4]],
+#  residuals_plots_list[[5]],
+#  residuals_plots_list[[6]],
+#  # residuals_plots_list[[7]],
+#  # legend_p,
+#  cols = 6
+#)
+#dev.off()
+#
+#### theta bar plot
+#theta_df$annotation <- factor(theta_df$annotation, levels = unique(theta_df$annotation))
+#theta_bar_plot <- ggplot(theta_df, aes(x = annotation, y = theta_value, fill = context)) +
+#  geom_bar(stat = "identity", position = position_dodge(width = 0.8), width = 0.6, color = "black") +
+#  geom_errorbar(aes(ymin = theta_value - theta_se, ymax = theta_value + theta_se), width = 0.2, position = position_dodge(0.8)) +
+#  scale_fill_brewer(palette = "Set3") +
+#  theme_bw() +
+#  labs(
+#    x = "",
+#    y = "ϴ",
+#    fill = "Context"
+#  ) +
+#  theme(axis.text.x = element_text(angle = 45, hjust = 1, face = "bold"))
+#
+#svg(paste0(path_2_save.2, "theta_values_", treatment, ".svg"), width = 5, height = 3, family = "serif")
+#print(theta_bar_plot)
+#dev.off()
+#
+#### R^2 bar plot
+#Rsqr_df$annotation <- factor(Rsqr_df$annotation, levels = unique(Rsqr_df$annotation))
+#end_brk_1 <- max(Rsqr_df[Rsqr_df$annotation != "TEGs", ]$Rsqr_value)
+#start_brk_2 <- min(c(Rsqr_df[Rsqr_df$annotation == "TEGs", ]$Rsqr_value))
+#
+#Rsqr_bar_plot <- ggplot(Rsqr_df, aes(x = annotation, y = Rsqr_value, fill = context)) +
+#  geom_bar(stat = "identity", position = position_dodge(width = 0.8), width = 0.6, color = "black") +
+#  scale_fill_brewer(palette = "Set3") +
+#  theme_bw() +
+#  labs(
+#    x = "",
+#    y = "R²",
+#    fill = "Context"
+#  ) +
+#  theme(axis.text.x = element_text(angle = 45, hjust = 1, face = "bold")) +
+#  ggbreak::scale_y_break(
+#    c(end_brk_1 * 1.075, start_brk_2 * 0.925),
+#    space = 0.055,
+#    scales = 0.5
+#  )
+#
+#svg(paste0(path_2_save.2, "Rsqr_values_", treatment, ".svg"), width = 5, height = 3, family = "serif")
+#print(Rsqr_bar_plot)
+#dev.off()
+#
+#### Pseudo-R^2 bar plot
+#ps_Rsqr_df$annotation <- factor(ps_Rsqr_df$annotation, levels = unique(ps_Rsqr_df$annotation))
+#end_brk_1 <- max(ps_Rsqr_df[ps_Rsqr_df$annotation != "TEGs", ]$Rsqr_value)
+#start_brk_2 <- min(c(ps_Rsqr_df[ps_Rsqr_df$annotation == "TEGs", ]$Rsqr_value))
+#
+#ps_Rsqr_bar_plot <- ggplot(ps_Rsqr_df, aes(x = annotation, y = Rsqr_value, fill = context)) +
+#  geom_bar(stat = "identity", position = position_dodge(width = 0.8), width = 0.6, color = "black") +
+#  scale_fill_brewer(palette = "Set3") +
+#  theme_bw() +
+#  labs(
+#    x = "",
+#    y = "Pseudo-R²",
+#    fill = "Context"
+#  ) +
+#  theme(axis.text.x = element_text(angle = 45, hjust = 1, face = "bold")) +
+#  ggbreak::scale_y_break(
+#    c(end_brk_1 * 1.075, start_brk_2 * 0.925),
+#    space = 0.055,
+#    scales = 0.5
+#  )
+#
+#svg(paste0(path_2_save.2, "Pseudo_Rsqr_values_", treatment, ".svg"), width = 5, height = 3, family = "serif")
+#print(ps_Rsqr_bar_plot)
+#dev.off()
+#
