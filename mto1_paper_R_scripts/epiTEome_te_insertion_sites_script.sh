88bdee341d3a31a4cba52a50f0194c69b2d1f4cb
Yonatan Yerushalmy <yonyerush@gmail.com>
2025-08-10 10:58:29 +0300

    te_trnasposition_scripts

--- mto1_paper_R_scripts/epiTEome_te_insertion_sites_script.sh
+++ mto1_paper_R_scripts/epiTEome_te_insertion_sites_script.sh
@@ -1,93 +1,104 @@
 #!/bin/bash
-
+#-------------------------------------------------------------
+# EpiTEome tool
+# https://github.com/jdaron/epiTEome
+# https://doi.org/10.1186/s13059-017-1232-0
 #-------------------------------------------------------------
 #
-### first run:
+# Usage: ./epiTEome_te_insertion_sites_script.sh [--dont_indx]
+#   --dont_indx          Skip genome indexing step
+#   --dont_concatenate   Skip concatenation of unmapped reads
+#   --run_test           Use test data
+#
+#-------------------------------------------------------------
+#
+# # first get unmapped reads using:
 # ./run_bismark_yo.sh -s ./mto1_samples_bismark.txt -g /home/yoyerush/yo/methylome_pipeline/Bismark/res_unmapped_040825/TAIR10_chr_all.fas.gz -o bismark_results -n 32 -m 16G --um
 #
 #-------------------------------------------------------------
 #
-### run to create retro-TEs list:
-# awk '$9 ~ /sF=LTR\/Copia|sF=LTR\/Gypsy|sF=LINE\/L1/ {
-#     split($9, arr, ";");
-#     for (i in arr) {
-#         if (arr[i] ~ /ID=/) {
-#             split(arr[i], id, "=");
-#             print id[2];
-#         }
-#     }
-# }' tair10TEs.gff3 | sort | uniq > retrotransposons_list.txt
-#
-### run to create helitron - "ATREP9" family list:
-# awk '$9 ~ /fam=ATREP9/ {
-#     split($9, arr, ";");
-#     for (i in arr) {
-#         if (arr[i] ~ /ID=/) {
-#             split(arr[i], id, "=");
-#             print id[2];
-#         }
-#     }
-# }' tair10TEs.gff3 | sort | uniq > helitron_atrep9_list.txt
-#
-### run to create copia superfamily list:
-# awk '$9 ~ /sF=LTR\/Copia/ {
-#     split($9, arr, ";");
-#     for (i in arr) {
-#         if (arr[i] ~ /ID=/) {
-#             split(arr[i], id, "=");
-#             print id[2];
-#         }
-#     }
-# }' tair10TEs.gff3 | sort | uniq > copia_list.txt
-#
-### run to create gypsy superfamily list:
-# awk '$9 ~ /sF=LTR\/Gypsy/ {
-#     split($9, arr, ";");
-#     for (i in arr) {
-#         if (arr[i] ~ /ID=/) {
-#             split(arr[i], id, "=");
-#             print id[2];
-#         }
-#     }
-# }' tair10TEs.gff3 | sort | uniq > gypsy_list.txt
-#
-### run to create LIN!/L1 superfamily list:
-# awk '$9 ~ /sF=LINE\/L1/ {
-#     split($9, arr, ";");
-#     for (i in arr) {
-#         if (arr[i] ~ /ID=/) {
-#             split(arr[i], id, "=");
-#             print id[2];
-#         }
-#     }
-# }' tair10TEs.gff3 | sort | uniq > line_l1_list.txt
+# # if there is error writing: << Unknown command 'filter' >>
+# # then modify the 'bamutils' script. example in 'install_rpiTEome_env.sh' script
 #
 #-------------------------------------------------------------
 
 # epiTEome scripts dir - also the output files path
-cd /home/yoyerush/yo/methylome_pipeline/transposition_activity/
-
+cd /home/yoyerush/yo/methylome_pipeline/transposition_activity
 samples_name=("mto1_1" "mto1_2" "mto1_3" "wt_1" "wt_2")
-
-te_ids_list=copia_list.txt
-
+te_super_family=copia
+te_ids_list=te_lists/copia_list.txt
 bismark_results=/home/yoyerush/yo/methylome_pipeline/Bismark/res_unmapped_040825/bismark_results
+genome_file=genome_indx/TAIR10_chr_all.epiTEome.masked.fasta
+gff_file=genome_indx/tair10TEs.gff3
+n_cores=30
 
 #-------------------------------------------------------------
 
-# index the genome
-perl ./idxEpiTEome.pl -l 150 -gff tair10TEs.gff3 -t $te_ids_list -fasta TAIR10_chr_all.fna
-
-mkdir -p unmapped_reads
-
-# Loop over samples
-for sample in "${samples_name[@]}"; do
-    # concatenate paired unmapped reads
-    zcat $bismark_results/"$sample"/"$sample"_unmapped_reads_1.fq.gz $bismark_results/"$sample"/"$sample"_unmapped_reads_2.fq.gz > unmapped_reads/"$sample"_unmapped_reads.fq
-    
-    # run epiTEome
-    perl ./epiTEome.pl -gff tair10TEs.gff3 -t $te_ids_list -ref TAIR10_chr_all.epiTEome.masked.fasta -un unmapped_reads/"$sample"_unmapped_reads.fq -p 20
-    
+# '--dont_concatenate' arguments
+dont_concatenate=0
+for arg in "$@"; do
+    if [[ "$arg" == "--dont_concatenate" ]]; then
+        dont_concatenate=1
+        break
+    fi
 done
 
-# gzip unmapped_reads/*_unmapped_reads.fq
+# '--dont_indx' arguments
+dont_indx=0
+for arg in "$@"; do
+    if [[ "$arg" == "--dont_indx" ]]; then
+        dont_indx=1
+        break
+    fi
+done
+
+# '--run_test' arguments
+run_test=0
+for arg in "$@"; do
+    if [[ "$arg" == "--run_test" ]]; then
+        run_test=1
+        break
+    fi
+done
+
+#-------------------------------------------------------------
+
+mkdir -p results
+
+if [[ $run_test -eq 0 ]]; then
+    
+    # index the genome
+    if [[ $dont_indx -eq 0 ]]; then
+        perl epiteome_scripts/idxEpiTEome.pl -l 150 -gff genome_indx/tair10TEs.gff3 -t $te_ids_list -fasta genome_indx/TAIR10_chr_all.fna
+    fi
+    
+    mkdir -p unmapped_reads
+    
+    # Loop over samples
+    for sample in "${samples_name[@]}"; do
+        
+        # concatenate paired unmapped reads
+        if [[ $dont_concatenate -eq 0 ]]; then
+            zcat $bismark_results/"$sample"/"$sample"_unmapped_reads_1.fq.gz $bismark_results/"$sample"/"$sample"_unmapped_reads_2.fq.gz > unmapped_reads/"$sample"_unmapped_reads.fq
+        fi
+        
+        mkdir -p results/"$sample"
+        cp unmapped_reads/"$sample"_unmapped_reads.fq results/"$sample"
+        
+        # run epiTEome
+        perl epiteome_scripts/epiTEome_yo_2.pl -gff $gff_file -t $te_ids_list -ref $genome_file -un results/"$sample"/"$sample"_unmapped_reads.fq -p $n_cores
+        
+        
+        mkdir -p results_"$te_super_family"
+        ### fix this shit ###
+        ### mv unmapped_reads/"$sample"unmapped.newInsertionSite.tab, unmapped.newInsertionSite.sam, unmapped.met.meta.tab and unmapped.met.row.tab
+    done
+    
+else
+    mkdir results/test_data_res
+    mkdir results/test_data_res/genome_indx_test
+    cp test_data/unmapped.fastq.bz2 results/test_data_res
+    cp test_data/Chr2.fasta results/test_data_res/genome_indx_test
+    perl epiteome_scripts/idxEpiTEome.pl -l 150 -gff genome_indx/tair10TEs.gff3 -t test_data/teid.lst -fasta results/test_data_res/genome_indx_test/Chr2.fasta
+    perl epiteome_scripts/epiTEome_yo_2.pl -gff $gff_file -t test_data/teid.lst -ref results/test_data_res/genome_indx_test/Chr2.fasta -un results/test_data_res/unmapped.fastq.bz2 -p 1
+fi
---
+++ scripts/install_epiTEome_env.sh
@@ -0,0 +1,363 @@
+
+
+################## if it worked with original samtools, write here the other installtion (with mamba and this shit
+################## also change the main script!!!!
+################## also take rpiTEome.pl from the 'no_samtools' directory. add '--threads'eith the ncores from the perl script
+################## no_samtools its mean that in the installation i didnt install samtools, i used the one in darwin. then the perl script edited just to use samtools for filtering instead of the 'ngsutils' python package
+
+#### install the damn epiTEome env
+conda create -y -n epiteome_core_mamba_py3
+conda activate epiteome_core_mamba_py3
+conda install -y -c conda-forge mamba
+
+mamba install -y -c conda-forge python=3.11 perl=5.32
+mamba install -y -c bioconda samtools bedtools=2.30 segemehl=0.2.0 perl-app-cpanminus perl-bioperl perl-bio-samtools perl-set-intervaltree perl-statistics-descriptive
+
+pip install ngsutils==0.5.7
+
+### change all py2 files into py3 <<< as the epiTEome deloper said: "Die Ficker!" >>>
+2to3 -w /home/yoyerush/anaconda3/envs/epiteome_core_mamba_py3/lib/python3.11/site-packages/ngsutils/
+
+#------------------------------------------
+
+cd /home/yoyerush/yo/methylome_pipeline/transposition_activity/
+
+mkdir epiteome_scripts
+cd epiteome_scripts
+wget https://raw.githubusercontent.com/jdaron/epiTEome/master/epiTEome.pl
+wget https://raw.githubusercontent.com/jdaron/epiTEome/master/idxEpiTEome.pl
+wget https://raw.githubusercontent.com/jdaron/epiTEome/master/insertTEsintoFasta.pl
+chmod +x ./epiTEome.pl
+chmod +x ./idxEpiTEome.pl
+chmod +x ./insertTEsintoFasta.pl
+cd ../
+
+mkdir genome_indx
+cd genome_indx
+wget -O TAIR10_chr_all.fna.gz https://www.arabidopsis.org/api/download-files/download?filePath=Genes/TAIR10_genome_release/TAIR10_chromosome_files/TAIR10_chr_all.fas.gz
+gunzip TAIR10_chr_all.fna.gz
+wget https://raw.githubusercontent.com/jdaron/epiTEome/master/test/tair10TEs.gff3
+cd ../
+
+mkdir test_data
+cd test_data
+wget https://raw.githubusercontent.com/jdaron/epiTEome/master/test/teid.lst
+wget https://raw.githubusercontent.com/jdaron/epiTEome/master/test/Chr2.fasta
+wget https://raw.githubusercontent.com/jdaron/epiTEome/master/test/unmapped.fastq.bz2
+cd ../
+
+#------------------------------------------
+
+### create target list (TE IDs list)
+mkdir te_lists
+cd te_lists
+# run to create retro-TEs list:
+awk '$9 ~ /sF=LTR\/Copia|sF=LTR\/Gypsy|sF=LINE\/L1/ {
+     split($9, arr, ";");
+     for (i in arr) {
+         if (arr[i] ~ /ID=/) {
+             split(arr[i], id, "=");
+             print id[2];
+         }
+     }
+}' ../genome_indx/tair10TEs.gff3 | sort | uniq > retrotransposons_list.txt
+
+# run to create helitron - "ATREP9" family list:
+awk '$9 ~ /fam=ATREP9/ {
+     split($9, arr, ";");
+     for (i in arr) {
+         if (arr[i] ~ /ID=/) {
+             split(arr[i], id, "=");
+             print id[2];
+         }
+     }
+}' ../genome_indx/tair10TEs.gff3 | sort | uniq > helitron_atrep9_list.txt
+
+# run to create copia superfamily list:
+awk '$9 ~ /sF=LTR\/Copia/ {
+     split($9, arr, ";");
+     for (i in arr) {
+         if (arr[i] ~ /ID=/) {
+             split(arr[i], id, "=");
+             print id[2];
+         }
+     }
+}' ../genome_indx/tair10TEs.gff3 | sort | uniq > copia_list.txt
+
+# run to create gypsy superfamily list:
+awk '$9 ~ /sF=LTR\/Gypsy/ {
+     split($9, arr, ";");
+     for (i in arr) {
+         if (arr[i] ~ /ID=/) {
+             split(arr[i], id, "=");
+             print id[2];
+         }
+     }
+}' ../genome_indx/tair10TEs.gff3 | sort | uniq > gypsy_list.txt
+
+# run to create LIN!/L1 superfamily list:
+awk '$9 ~ /sF=LINE\/L1/ {
+     split($9, arr, ";");
+     for (i in arr) {
+         if (arr[i] ~ /ID=/) {
+             split(arr[i], id, "=");
+             print id[2];
+         }
+     }
+}' ../genome_indx/tair10TEs.gff3 | sort | uniq > line_l1_list.txt
+
+cd ../
+
+#------------------------------------------
+
+###################################
+## if there is error writing: << Unknown command 'filter' >>
+#  /home/yoyerush/anaconda3/envs/epiteome_core_mamba_py3/bin/bamutils
+#  or find it with the command << which bamutils >>
+#  note: also to fastqutils
+#
+#  then make this modifications inside the bamutil file:
+
+#  #!/bin/sh
+#  REAL=/home/yoyerush/anaconda3/envs/epiteome_core_mamba_py3/lib/python3.11/site-packages/ngsutils
+#  DIR=`dirname "$REAL"`/../
+#  SUBDIR=/home/yoyerush/anaconda3/envs/epiteome_core_mamba_py3/lib/python3.11/site-packages/ngsutils/bam/
+# 
+#  usage() {
+#      echo "Usage: $(basename $0) COMMAND"
+#      echo ""
+#      cat $DIR/ngsutils/$SUBDIR/README
+#      echo ""
+#      echo "Run '$(basename $0) help CMD' for more information about a specific command"
+#      echo -n "ngsutils "
+# 
+#      cd $DIR
+#      GV="$(git show master --format='%h %ai' | head -n 1)"
+#      VERSION=$(echo "$GV" | awk '{print $1}')
+#      echo "$(cat VERSION | sed -e 's/\n//')-$VERSION"
+# 
+#      exit 1
+#  }
+# 
+#  if [ "$1" = "" ]; then
+#      usage
+#  fi
+# 
+# 
+#  . "/home/yoyerush/anaconda3/pkgs/python-3.7.6-h0371630_2/lib/python3.7/venv/scripts/common/activate"
+# 
+#  export PYTHONPATH=$PYTHONPATH:"$DIR"
+# 
+#  if [ "$SUBDIR" = "ngs" ]; then
+#      if [ -e "$DIR"/.git ]; then
+#          if [ "$1" = "update" ]; then
+#              cd "$DIR"
+# 
+#              echo "Updating from current branch"
+#              git pull
+# 
+#              exit 0
+#          elif [ "$1" = "switch" ]; then
+#              cd "$DIR"
+# 
+#              if [ "$2" != "" ]; then
+#                  echo "Switching to branch: $2"
+#                  git fetch origin
+#                  if [ "$(git branch -l | grep "$2")" = "" ]; then
+#                      git branch -t $2 origin/$2
+#                  fi
+#                  git checkout $2
+#                  git pull
+#              else
+#                  git branch -a
+#              fi
+#              exit 0
+#          fi
+#      fi
+#  fi
+# 
+# 
+#  if [ "$1" = "help" ]; then
+#      if [ "$2" = "" ]; then
+#          usage
+#      fi
+# 
+#      action=/home/yoyerush/anaconda3/envs/epiteome_core_mamba_py3/lib/python3.11/site-packages/ngsutils/bam/filter.py
+# 
+#      if [ ! -e "$action" ]; then
+#          action="$DIR"/ngsutils/$SUBDIR/$2.sh
+#          if [ ! -e "$action" ]; then
+#              echo "Unknown command '$2'"
+#              exit 1
+#          fi
+#      fi
+#      "$action" -h
+# 
+#  elif [ "$1" = "version" ]; then
+#      cd $DIR
+#      GV="$(git show master --format='%h %ai' | head -n 1)"
+#      VERSION=$(echo "$GV" | awk '{print $1}')
+#      echo "$(cat VERSION | sed -e 's/\n//')-$VERSION"
+# 
+#  elif [ "$1" = "profile" ]; then
+#      shift
+#      action=/home/yoyerush/anaconda3/envs/epiteome_core_mamba_py3/lib/python3.11/site-packages/ngsutils/bam/filter.py
+# 
+#      if [ ! -e "$action" ]; then
+#          action="$DIR"/ngsutils/$SUBDIR/filter.sh
+#          if [ ! -e "$action" ]; then
+#              echo "Unknown command 'filter'"
+#              exit 1
+#          fi
+#      fi
+#      shift
+# 
+#  #    i=0
+#  #    for arg in "$@"; do
+#  #        ARGS[$i]="$arg"
+#  #        ((++i))
+#  #    done
+# 
+#      echo "Saving profile information to profile.output" 1>&2
+#      exec python -m cProfile -o profile.output "$DIR"/ngsutils/$SUBDIR/$action "$@"
+#  else
+#      action=/home/yoyerush/anaconda3/envs/epiteome_core_mamba_py3/lib/python3.11/site-packages/ngsutils/bam/filter.py
+# 
+#      if [ ! -e "$action" ]; then
+#          action="$DIR"/ngsutils/$SUBDIR/filter.sh
+#          if [ ! -e "$action" ]; then
+#              echo "Unknown command '$1'"
+#              exit 1
+#          fi
+#      fi
+#      shift
+# 
+#      exec "$action" "$@"
+#  fi
+
+
+#### edit fastqutils
+#  #!/bin/bash
+#  REAL=/home/yoyerush/anaconda3/envs/epiteome_core_mamba_py3/lib/python3.11/site-packages/ngsutils
+#  DIR=`dirname "$REAL"`/../
+#  SUBDIR=$(basename $0 | sed -e 's/utils//')
+#  
+#  function usage() {
+#      echo "Usage: $(basename $0) COMMAND"
+#      echo ""
+#      cat $DIR/ngsutils/$SUBDIR/README
+#      echo ""
+#      echo "Run '$(basename $0) help CMD' for more information about a specific command"
+#      echo -n "ngsutils "
+#      
+#      cd $DIR
+#      GV="$(git show master --format='%h %ai' | head -n 1)"
+#      VERSION=$(echo "$GV" | awk '{print $1}')
+#      echo "$(cat VERSION | sed -e 's/\n//')-$VERSION"
+#  
+#      exit 1
+#  }
+#  
+#  if [[ "$1" == "" ]]; then
+#      usage
+#  fi
+#  
+#  
+#  . "/home/yoyerush/anaconda3/pkgs/python-3.7.6-h0371630_2/lib/python3.7/venv/scripts/common/activate"
+#  
+#  export PYTHONPATH=$PYTHONPATH:"$DIR"
+#  
+#  if [[ "$SUBDIR" == "ngs" ]]; then
+#      if [[ -e "$DIR"/.git ]]; then
+#          if [[ "$1" == "update" ]]; then
+#              cd "$DIR"
+#      
+#              echo "Updating from current branch"
+#              git pull 
+#  
+#              exit 0
+#          elif [[ "$1" == "switch" ]]; then
+#              cd "$DIR"
+#      
+#              if [[ "$2" != "" ]]; then
+#                  echo "Switching to branch: $2"
+#                  git fetch origin
+#                  if [[ "$(git branch -l | grep "$2")" == "" ]]; then
+#                      git branch -t $2 origin/$2
+#                  fi
+#                  git checkout $2
+#                  git pull
+#              else
+#                  git branch -a
+#              fi
+#              exit 0
+#          fi
+#      fi
+#  fi
+#  
+#  
+#  if [[ "$1" == "help" ]]; then
+#      if [[ "$2" == "" ]]; then
+#          usage
+#      fi
+#      
+#      action=$2.py
+#      
+#      if [[ ! -e "$DIR"/ngsutils/$SUBDIR/$action ]]; then
+#          action=$2.sh
+#          if [[ ! -e "$DIR"/ngsutils/$SUBDIR/$action ]]; then
+#              echo "Unknown command '$2'"
+#              exit 1
+#          fi
+#      fi
+#      "$DIR"/ngsutils/$SUBDIR/$action -h
+#  
+#  elif [[ "$1" == "version" ]]; then
+#      cd $DIR
+#      GV="$(git show master --format='%h %ai' | head -n 1)"
+#      VERSION=$(echo "$GV" | awk '{print $1}')
+#      echo "$(cat VERSION | sed -e 's/\n//')-$VERSION"
+#  
+#  elif [[ "$1" == "profile" ]]; then
+#      shift
+#      action=$1.py
+#  
+#      if [[ ! -e "$DIR"/ngsutils/$SUBDIR/$action ]]; then
+#          action=$1.sh
+#          if [[ ! -e "$DIR"/ngsutils/$SUBDIR/$action ]]; then
+#              echo "Unknown command '$1'"
+#              exit 1
+#          fi
+#      fi
+#      shift
+#  
+#      ARGS=()
+#      i=0
+#      for arg in "$@"; do
+#          ARGS[$i]="$arg"
+#          ((++i))
+#      done
+#      
+#      echo "Saving profile information to profile.output" 1>&2
+#      exec python -m cProfile -o profile.output "$DIR"/ngsutils/$SUBDIR/$action "${ARGS[@]}"
+#  else
+#      action=$1.py
+#  
+#      if [[ ! -e "$DIR"/ngsutils/$SUBDIR/$action ]]; then
+#          action=$1.sh
+#          if [[ ! -e "$DIR"/ngsutils/$SUBDIR/$action ]]; then
+#              echo "Unknown command '$1'"
+#              exit 1
+#          fi
+#      fi
+#      shift
+#  
+#      ARGS=()
+#      i=0
+#      for arg in "$@"; do
+#          ARGS[$i]="$arg"
+#          ((++i))
+#      done
+#      
+#      exec "$DIR"/ngsutils/$SUBDIR/$action "${ARGS[@]}"
+#  fi
