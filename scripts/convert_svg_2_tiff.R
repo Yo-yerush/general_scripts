c84674832288f875fdbdb05ad7fec46adc216c77
Yonatan Yerushalmy <yonyerush@gmail.com>
2025-10-07 12:50:25 +0300

    add_width

--- scripts/convert_svg_2_tiff.R
+++ scripts/convert_svg_2_tiff.R
@@ -1,4 +1,4 @@
-svg_2_tiff <- function(svg_path, dpi = 600, output_file = NULL) {
+svg_2_tiff <- function(svg_path, dpi = 600, output_file = NULL, width_mm = NULL, width_pixels = NULL) {
     suppressMessages(suppressWarnings(library(rsvg)))
     suppressMessages(suppressWarnings(library(magick)))
 
@@ -9,8 +9,21 @@ svg_2_tiff <- function(svg_path, dpi = 600, output_file = NULL) {
         output_file <- sub("\\.svg$", ".tiff", svg_path, ignore.case = TRUE)
     }
 
-    img <- image_read_svg(svg_path)
-    image_write(img, output_file, format = "tiff", density = dpi)
 
+    img <- image_read_svg(svg_path)
+
+    ####################
+    # resize by width
+    if (!is.null(width_mm) && is.null(width_pixels)) {
+        # convert mm to pixels based on dpi
+        width_pixels <- width_mm * (dpi / 25.4)
+        # width_pixels <- width_mm * 3.7795275591
+    }
+    if (!is.null(width_pixels)) {
+        # resize image to specified width while maintaining aspect ratio
+        img <- image_resize(img, paste0(width_pixels, "x"))
+    }
+
+    image_write(img, output_file, format = "tiff", density = dpi)
     cat("saved:", basename(output_file), "\n")
 }
