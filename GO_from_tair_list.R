04f5b1f9c509e3ff3b26e325c4ae7fa3845d1366
Yonatan Yerushalmy <yonyerush@gmail.com>
2025-10-30 11:10:22 +0200

    updated_GO_function

--- GO_from_tair_list.R
+++ GO_from_tair_list.R
@@ -1,9 +1,8 @@
-GO_from_tairList <- function(interesting.TAIRs, background.TAIRs="all", treatment = "TAIR list", ontology_type = "BP", pValue.threshold = 0.01, topGO.algorithm = "weight01", topGO.statistic = "fisher", netPlot = FALSE) {
-    
+GO_from_tairList <- function(interesting.TAIRs, background.TAIRs = "all", treatment = "TAIR list", ontology_type = "BP", pValue.threshold = 0.01, topGO.algorithm = "weight01", topGO.statistic = "fisher", netPlot = FALSE) {
     # this function run GO analysis for arabidopsis TAIR IDs
     # just insert the gene list to test (interesting.TAIRs)
     # and the beckground TAIR list (background.TAIRs)
-    # 
+    #
     # if using 'nePlot=TRUE', 'background.TAIRs' argument is not needed
     #
     # GO_from_tairList(interesting.TAIRs, background.TAIRs, pValue.threshold = 0.001)
@@ -15,43 +14,46 @@ GO_from_tairList <- function(interesting.TAIRs, background.TAIRs="all", treatmen
         library(ggplot2)
     })
 
-    msg <- paste("\n***************************\nontology type:", ontology_type, "\npadj threshold:", pValue.threshold, "\ntopGO algorithm:", topGO.algorithm, "\ntopGO statistic:", topGO.statistic,
-    ifelse(any(background.TAIRs == "all"), "\nbackground genes: all TAIR IDs",""),
-    "\n\n")
+    msg <- paste(
+        "\n***************************\nontology type:", ontology_type, "\npadj threshold:", pValue.threshold, "\ntopGO algorithm:", topGO.algorithm, "\ntopGO statistic:", topGO.statistic,
+        ifelse(
+            any(background.TAIRs == "all") & !netPlot,
+            "\nbackground genes: all TAIR IDs\n\n",
+            ifelse(netPlot, "\nNetPlot TAIRs count:", "\n\n")
+        )
+    )
 
     # all TAIR IDs (if not inserted beckground TAIR list)
     if (any(background.TAIRs == "all")) {
-    background.TAIRs <- keys(org.At.tair.db, keytype = "TAIR")
+        background.TAIRs <- keys(org.At.tair.db, keytype = "TAIR")
     }
 
 
     if (!netPlot) {
+        ###### GO analysis table
         # indicating if a gene is interesting (1) or not (0)
         geneList <- factor(as.integer(background.TAIRs %in% interesting.TAIRs))
         names(geneList) <- background.TAIRs
 
         # topGOdata object
         GOdata <- new("topGOdata",
-            ontology = ontology_type, # or "CC" or "MF", depending on your interest
+            ontology = ontology_type,
             allGenes = geneList,
             geneSelectionFun = function(x) (x == 1),
-            # nodeSize = 10, # Minimum number of genes for a GO term to be considered
+            # nodeSize = 10,
             annot = annFUN.org,
-            # geneSel = geneSelectionFun,
             mapping = "org.At.tair.db"
         )
 
         # run and output significants terms
         resultClassic <- runTest(GOdata, algorithm = topGO.algorithm, statistic = topGO.statistic)
-        allResClassic <- GenTable(GOdata, pValue = resultClassic, topNodes = length(resultClassic@score)) # topNodes can be adjusted
+        allResClassic <- GenTable(GOdata, pValue = resultClassic, topNodes = length(resultClassic@score))
         allResClassic_sig <- allResClassic[allResClassic$pValue <= pValue.threshold, ]
 
         cat(msg)
         return(allResClassic_sig)
-
     } else {
-        ##################################
-        # Net-Plot
+        ###### Net-Plot
         suppressMessages(library(clusterProfiler))
 
         cat("netPlot...\n")
@@ -65,12 +67,14 @@ GO_from_tairList <- function(interesting.TAIRs, background.TAIRs="all", treatmen
             qvalueCutoff = 0.2
         )
         ego_sig <- ego@result[ego@result$p.adjust < pValue.threshold, ]
-        cat(msg)
+        cat(msg, nrow(ego_sig), "\n\n")
 
-        if (nrow(ego_sig) < 5) {
-            title_size <- 10
+        title_size <- if (nrow(ego_sig) <= 10) {
+            20
+        } else if (nrow(ego_sig) > 100) {
+            nrow(ego_sig) / 10
         } else {
-            title_size <- nrow(ego_sig) * 2
+            nrow(ego_sig) * 2
         }
 
         if (nrow(ego_sig) != 0) {
